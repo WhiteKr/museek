d73b1b93681672211b6b81034f71903dd5f8966d
WhiteKr <hanjisu2215g@gmail.com>
2023-12-10 07:17:42 +0900

    Configure dynamic commands distructure; with a command Ping

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -6,7 +6,7 @@ import { z } from 'zod';
 let envSchema = z.object({
   BOT_TOKEN: z.string().min(1),
   CLIENT_ID: z.string().min(1),
-  DEV_GUILD_ID: z.string().min(1),
+  GUILD_ID: z.string().min(1),
 });
 
 configDotenv();
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,50 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import * as fs from 'fs';
+import * as path from 'path';
 import { env } from './configs/env';
 
 const client: Client<boolean> = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
 
-client.once(Events.ClientReady, (readyClient: Client<true>) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+client.commands = new Collection();
+
+const foldersPath: string = path.join(__dirname, 'commands');
+const commandFolders: string[] = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath: string = path.join(foldersPath, folder);
+  const commandFiles: string[] = fs
+    .readdirSync(commandsPath)
+    .filter((file: string) => file.endsWith('.js'));
+
+  for (const file of commandFiles) {
+    const filePath: string = path.join(commandsPath, file);
+    const command: any = require(filePath);
+
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
+
+const eventsPath: string = path.join(__dirname, 'events');
+const eventFiles: string[] = fs
+  .readdirSync(eventsPath)
+  .filter((file: string) => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const filePath: string = path.join(eventsPath, file);
+  const event: any = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args: any) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args: any) => event.execute(...args));
+  }
+}
 
 client.login(env.BOT_TOKEN);
