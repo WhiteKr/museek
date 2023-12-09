57033399f33aa794d3e5fc03b04c8c5a6ea102ae
WhiteKr <hanjisu2215g@gmail.com>
2023-12-10 07:29:15 +0900

    Add dev mark on description of commands for development

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -33,11 +33,15 @@ const getCommands = (): any[] => {
 
 export const deployCommands = async (): Promise<void> => {
   const commands: any[] = getCommands();
+  const devCommands: any[] = commands.map((command: any) => ({
+    ...command,
+    description: `${command.description} (dev)`,
+  }));
 
   const rest: REST = new REST().setToken(env.BOT_TOKEN);
   await rest.put(Routes.applicationCommands(env.CLIENT_ID), { body: commands });
   await rest.put(Routes.applicationGuildCommands(env.CLIENT_ID, env.GUILD_ID), {
-    body: commands,
+    body: devCommands,
   });
   console.log(
     `Successfully reloaded ${commands.length} application (/) commands.`
