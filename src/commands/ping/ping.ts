f7a3de147427269c78e58f297d2304a938ad805f
WhiteKr <hanjisu2215g@gmail.com>
2023-12-10 20:56:15 +0900

    Ping-pong ephemeral selectable

--- src/commands/ping/ping.ts
+++ src/commands/ping/ping.ts
@@ -1,12 +1,23 @@
-import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
+import {
+  ChatInputCommandInteraction,
+  SlashCommandBooleanOption,
+  SlashCommandBuilder,
+} from 'discord.js';
 import { CustomCommand } from '../../@types/custom';
 
 module.exports = {
   cooldown: 5,
   data: new SlashCommandBuilder()
     .setName('ping')
-    .setDescription('Replies with Pong!'),
-  async execute(interaction: CommandInteraction): Promise<void> {
-    await interaction.reply('Pong!');
+    .setDescription('Replies with Pong!')
+    .addBooleanOption((option: SlashCommandBooleanOption) =>
+      option
+        .setName('secret')
+        .setDescription('Make this ping-pong a secret just between you and me!')
+    ),
+  async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+    const isSecret: boolean =
+      interaction.options.getBoolean('secret', false) ?? false;
+    await interaction.reply({ content: 'Pong!', ephemeral: isSecret });
   },
 } as CustomCommand;
